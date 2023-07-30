238e166c136b237ba21a9f958453db2c4bfe0adb
Arvind Rachuri <arvind.rachuri@oracle.com>
2023-07-30 16:14:53 +0530

    resolve rebase conflicts

--- assignment-flipkart/src/App.js
+++ assignment-flipkart/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+
 import EmailListItem from './components/email-list-item/email-list-item-component';
+import EmailDetails from './components/email-detail-component/email-detail-component';
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
         snippet="Looking forward to seeing you there!"
         timestamp="2 days ago"
       />
+      <EmailDetails senderInitial={"F"} subject={"Lorem Ipsum"} date={"26/10/20 10:30am"}/>
     </div>
   );
 }
